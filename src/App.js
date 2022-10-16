f2c02f7e32d0b3389921d9be7ef77d7be98fe9f8
Neitaddi <69628774+Neitaddi@users.noreply.github.com>
2022-10-16 18:39:23 +0200

    hello

--- src/App.js
+++ src/App.js
@@ -1,90 +1,79 @@
-import './App.css';
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-export default class App extends React.Component{
-    
-  constructor(props){
-    super(props)
-    this.state={
-     
-        time: 0,
-        show: false,
-        x: 'Show',
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 0,
+      show: false,
+      x: "Show",
       Person: {
         fullName: "Neit Addi Mariem",
         bio: "GoMyCode student",
-        imgSrc: "mariem.jpeg",
+        imgSrc: "mariem.jpg",
         profession: "full-stack developer",
-        
-      }
+      },
+    };
+  }
+
+  handleClick = () => {
+    this.setState({ show: !this.state.show, time: 0 });
+    if (this.state.show === false) {
+      this.setState({ show: true });
+      this.setState({ x: "Hide" });
+    } else if (this.state.show === true) {
+      this.setState({ show: false });
+      this.setState({ x: "Show" });
     }
+  };
 
-  }
-
-
-
-  handleClick = () =>{ this.setState({show: !this.state.show , time:0});
-  if (this.state.show===false) {
-    this.setState({show: true});
-    this.setState({x: 'Hide'});
-  }
-  else if (this.state.show===true) {
-    this.setState({show: false}); 
-    this.setState({x: 'Show'});
-  }
-}
-  
-  componentDidMount(){
-    setInterval( ()=> {
+  componentDidMount() {
+    setInterval(() => {
       this.setState({
-        time: this.state.time +1
-      })
-    } ,1000  )
+        time: this.state.time + 1,
+      });
+    }, 1000);
   }
   render() {
-    const {fullName, bio, imgSrc, profession} = this.state.Person;
+    const { fullName, bio, imgSrc, profession } = this.state.Person;
 
-  return (
+    return (
+      <div className="all">
+        <div className={this.state.show ? "visible" : "hide"}>
+          <div className="container-fluid">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="row">
+                  <div class="col-md-6">
+                    <img src={imgSrc} />
+                  </div>
+                  <div class="col-md-6">
+                    <p> Hi ! my name is {fullName}</p>
 
-<div className="all">
-<div className={this.state.show ? "visible" : "hide"}>
-   <div className="container-fluid">
-	<div class="row">
-		<div class="col-md-12">
-			<div class="row">
-				<div class="col-md-6">
-        <img   src={imgSrc}  />
-				</div>
-				<div class="col-md-6">
-         <p > Hi ! my name is {fullName}</p>
-     
-         <p >  {bio}</p>
-         <p >I'm <span>{profession}</span></p>
-           <h2><span class="noselect">{this.state.time}</span><div id="circle"></div>
-</h2>
-          
-
-           
-			</div>
-		</div>
-	</div>
-  </div>
-  </div>
-  </div>
-
-  <div className="visible">
-            <div id="btn"  onClick={this.handleClick}>
-        <span class="noselect">{this.state.x}</span>
-        <div id="circle"></div></div>
-				</div>
-       
-
-      
-        
+                    <p> {bio}</p>
+                    <p>
+                      I'm <span>{profession}</span>
+                    </p>
+                    <h2>
+                      <span class="noselect">{this.state.time}</span>
+                      <div id="circle"></div>
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    
-    )
+
+        <div className="visible">
+          <div id="btn" onClick={this.handleClick}>
+            <span class="noselect">{this.state.x}</span>
+            <div id="circle"></div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
-
